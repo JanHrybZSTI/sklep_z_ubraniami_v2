0529f8513b6541d1e3dc23168c6e93234b157036
JanHrybZSTI <73634494+JanHrybZSTI@users.noreply.github.com>
2020-12-02 13:04:36 +0100

    finally made links

--- scripts/code.js
+++ scripts/code.js
@@ -21,3 +21,16 @@ hamburgerX.addEventListener("click", function () {
   document.styleSheets[3].disabled = false;
   document.styleSheets[2].disabled = false;
 });
+
+//
+
+shadow = document.querySelectorAll(".shadow");
+
+for (let i = 0; i < shadow.length; i++) {
+  shadow[i].addEventListener("mouseover", () => {
+    shadow[i].style.boxShadow = "inset 0 0 500px 250px black";
+  });
+  shadow[i].addEventListener("mouseout", () => {
+    shadow[i].style.boxShadow = "none";
+  });
+}
